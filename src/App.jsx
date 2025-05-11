ed51b0701150f933ba524917a2f2a745817fd494
TrangDangDucTin <115961028+trangdangductin@users.noreply.github.com>
2025-05-11 14:59:26 +0700

    theme added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
 import HomeIcon from '@mui/icons-material/Home';
 import { pink } from '@mui/material/colors';
+import Typography from '@mui/material/Typography';
+
 function App() {
   
 
   return (
     <>
     <div>kaguya</div>
+    <Typography variant="body2" color="text.secondary"> TEst </Typography>
     <Button variant="text">Text</Button>
     <Button variant="contained">Contained</Button>
     <Button variant="outlined">Outlined</Button>
